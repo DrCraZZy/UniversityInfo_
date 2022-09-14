dd8cdad9f53420f2f24616a418ec513864f654d8
maxim <maxim.bienemann@gmail.com>
2022-09-14 11:13:54 +0200

    Typo

--- src/main/java/com/example/compare/universities/CompareUniversitiesByName.java
+++ src/main/java/com/example/compare/universities/CompareUniversitiesByName.java
@@ -6,6 +6,6 @@ import org.apache.commons.lang3.StringUtils;
 public class CompareUniversitiesByName implements CompareUniversities{
     @Override
     public int compare(University university1, University university2) {
-        return StringUtils.compare(university1.getFullName(), university1.getFullName());
+        return StringUtils.compare(university1.getFullName(), university2.getFullName());
     }
 }
