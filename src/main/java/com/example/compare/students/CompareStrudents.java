61ea92affcefe7ce47c700352071bcc784abb3a1
maxim <maxim.bienemann@gmail.com>
2022-09-14 09:25:20 +0200

    Create Comparator-Interfaces for Student and University classes

---
+++ src/main/java/com/example/compare/students/CompareStrudents.java
@@ -0,0 +1,8 @@
+package com.example.compare.students;
+
+import com.example.model.Student;
+
+import java.util.Comparator;
+
+public interface CompareStrudents extends Comparator<Student> {
+}
---
+++ src/main/java/com/example/compare/universities/CompareUniversities.java
@@ -0,0 +1,9 @@
+package com.example.compare.universities;
+
+import com.example.model.University;
+
+import java.util.Comparator;
+
+public interface CompareUniversities extends Comparator<University> {
+
+}
