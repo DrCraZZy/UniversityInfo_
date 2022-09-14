40abdaebb5c13468c28b42b0a7a7dbab8c3b9f2e
maxim <maxim.bienemann@gmail.com>
2022-09-14 11:15:40 +0200

    Main with new "tests"

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,34 @@
 package com.example;
 
-import com.example.enums.StudyProfile;
+import com.example.compare.students.CompareStudents;
+import com.example.compare.universities.CompareUniversities;
+import com.example.enums.StudentComparator;
+import com.example.enums.UniversityComparator;
 import com.example.model.Student;
 import com.example.model.University;
+import com.example.reader.XlsxReader;
+
+import java.util.List;
+
+import static com.example.compare.UtilComparator.getStudentComparator;
+import static com.example.compare.UtilComparator.getUniversityComparator;
 
 public class Main
 {
     public static void main( String[] args )
     {
-        University university = new University("H", "Hogwords", "Hog", 1997, StudyProfile.MAGIC);
-        System.out.println(university);
+        String path = "src/main/resources/universityInfo.xlsx";
 
-        Student student = new Student("Harry Potter", "H", 1, 1);
-        System.out.println(student);
+        XlsxReader xlsxReader = new XlsxReader(path);
+        List<University> universities = xlsxReader.readUniversity();
+
+        CompareUniversities compareUniversities = getUniversityComparator(UniversityComparator.BY_ID);
+        universities.stream().sorted(compareUniversities).forEach(System.out::println);
+
+        System.out.println("--------------------------------------------------------");
+
+        List<Student> students = xlsxReader.readStudent();
+        CompareStudents compareStudents = getStudentComparator(StudentComparator.By_AVG_EXAM_SCORE_REVERSE);
+        students.stream().sorted(compareStudents).forEach(System.out::println);
     }
 }
