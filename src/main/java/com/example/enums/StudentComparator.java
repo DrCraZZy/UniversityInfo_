b424939c40bf760ef3076affb2137e6eb7efa8ef
maxim <maxim.bienemann@gmail.com>
2022-09-14 11:08:10 +0200

    Create Enums for Students and Universities with comparator possibilities.

---
+++ src/main/java/com/example/enums/StudentComparator.java
@@ -0,0 +1,10 @@
+package com.example.enums;
+
+public enum StudentComparator {
+    BY_NAME,
+    BY_CURRENT_COURSE_NUMBER,
+    BY_AVG_EXAM_SCORE,
+    By_AVG_EXAM_SCORE_REVERSE,
+    BY_UNIVERSITY_ID;
+
+}
---
+++ src/main/java/com/example/enums/UniversityComparator.java
@@ -0,0 +1,9 @@
+package com.example.enums;
+
+public enum UniversityComparator {
+    BY_ID,
+    BY_FULL_NAME,
+    BY_SHORT_NAME,
+    BY_YEAR_OF_FOUNDATION,
+    BY_MAIN_PROFILE;
+}
