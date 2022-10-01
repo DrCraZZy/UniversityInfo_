b057451983a63b9790b34ed9b3533004e9af05a6
maxim <maxim.bienemann@gmail.com>
2022-10-01 20:05:25 +0200

    Add new class to write to xlsx

---
+++ src/main/java/com/example/io/XlsWriter.java
@@ -0,0 +1,75 @@
+package com.example.io;
+
+import com.example.model.Statistic;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+public class XlsWriter {
+
+    private final String filePath;
+
+    public XlsWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void writeStatistics(List<Statistic> statistics) {
+
+        try(
+                OutputStream outputStream = new FileOutputStream(this.filePath);
+                Workbook workbook = new XSSFWorkbook()
+        ) {
+
+            Sheet sheet = workbook.createSheet("Statistics");
+
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            headerFont.setFontHeightInPoints((short) 14);
+            CellStyle headerStyle = workbook.createCellStyle();
+            headerStyle.setFont(headerFont);
+
+            int rowNum = 0;
+            Row row = sheet.createRow(rowNum++);
+            Cell profileCell = row.createCell(0);
+            profileCell.setCellValue("Profile");
+            profileCell.setCellStyle(headerStyle);
+
+            Cell avgScoreCell = row.createCell(1);
+            avgScoreCell.setCellValue("AVG Score");
+            avgScoreCell.setCellStyle(headerStyle);
+
+            Cell numberOfUniversitiesCell = row.createCell(2);
+            numberOfUniversitiesCell.setCellValue("Number of Universities");
+            numberOfUniversitiesCell.setCellStyle(headerStyle);
+
+            Cell numberOfStudentsCell = row.createCell(3);
+            numberOfStudentsCell.setCellValue("Number of Students");
+            numberOfStudentsCell.setCellStyle(headerStyle);
+
+            Cell universitiesNames = row.createCell(4);
+            universitiesNames.setCellValue("Universities names");
+            universitiesNames.setCellStyle(headerStyle);
+
+            if(statistics != null) {
+                for (Statistic s : statistics) {
+                    row = sheet.createRow(rowNum++);
+                    row.createCell(0).setCellValue(s.getProfile().toString());
+                    row.createCell(1).setCellValue(s.getAvgExamScore());
+                    row.createCell(2).setCellValue(s.getNumberOfUniversitiesInProfile());
+                    row.createCell(3).setCellValue(s.getNumberOfStudentsInProfile());
+                    row.createCell(4).setCellValue(s.getUniversitiesNames());
+                }
+            }
+
+            workbook.write(outputStream);
+
+        } catch (IOException e) {
+            System.err.println("There is a problem by write statistics to the file" + this.filePath);
+            throw new RuntimeException(e);
+        }
+    }
+}
