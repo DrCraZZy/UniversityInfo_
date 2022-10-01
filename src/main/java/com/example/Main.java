d269deaf35cd6a9e9986fcd370624bcc5d2973b1
maxim <maxim.bienemann@gmail.com>
2022-10-01 20:08:16 +0200

    Tests of new possibilities

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,73 +1,79 @@
 package com.example;
 
-import com.example.compare.students.CompareStudents;
-import com.example.compare.universities.CompareUniversities;
-import com.example.enums.StudentComparator;
-import com.example.enums.UniversityComparator;
+import com.example.io.XlsxReader;
+import com.example.io.XlsWriter;
+import com.example.model.Statistic;
 import com.example.model.Student;
 import com.example.model.University;
-import com.example.reader.XlsxReader;
+import com.example.util.CreateStatisticsUtil;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.example.compare.UtilComparator.getStudentComparator;
-import static com.example.compare.UtilComparator.getUniversityComparator;
 
 public class Main
 {
     public static void main( String[] args )
     {
-        String path = "src/main/resources/universityInfo.xlsx";
-        XlsxReader xlsxReader = new XlsxReader(path);
+        String pathUniversityInfo = "src/main/resources/universityInfo.xlsx";
+        String pathUniversityStatistics = "src/main/resources/universityStatistics.xlsx";
+        XlsxReader xlsxReader = new XlsxReader(pathUniversityInfo);
+
+        List<Student> students = xlsxReader.readStudent();
+        List<University> universities = xlsxReader.readUniversity();
+
+        CreateStatisticsUtil createStatistic = new CreateStatisticsUtil();
+        List<Statistic> statistics = createStatistic.createStatistics(students, universities);
+
+        XlsWriter xw = new XlsWriter(pathUniversityStatistics);
+
+       xw.writeStatistics(statistics);
 
 
-        CompareUniversities compareUniversities = getUniversityComparator(UniversityComparator.BY_ID);
-        List<University> universities = xlsxReader.readUniversity().stream().sorted(compareUniversities).collect(Collectors.toList());
-
-        // universities collection serialization (4)
-        String jsonUniversities = JsonUtil.UniversitiesToJson(universities);
-        System.out.println(jsonUniversities);
-
-        // jsonUniversities deserialization to new collection (5)
-        List<University> deserializedUniversitiesCollection = JsonUtil.UniversitiesFromJson(jsonUniversities);
-
-        // check number of elements in collections (before and after deserialization) (6)
-        if (universities.size() != deserializedUniversitiesCollection.size()) {
-            System.out.println("Number of objects before deserialization (" + universities.size() + ") and after (" +
-                    deserializedUniversitiesCollection.size() + ") are different.");
-        }
-
-        // Check ser/deser for single objects
-        universities.
-                stream().
-                map(JsonUtil::UniversityToJson).
-                map(JsonUtil::UniversityFromJson).
-                forEach(System.out::println);
-
-        System.out.println("--------------------------------------------------------");
-
-        CompareStudents compareStudents = getStudentComparator(StudentComparator.By_AVG_EXAM_SCORE_REVERSE);
-        List<Student> students = xlsxReader.readStudent().stream().sorted(compareStudents).collect(Collectors.toList());
-
-        // students collection serialization (4)
-        String jsonStudents = JsonUtil.StudentsToJson(students);
-        System.out.println(jsonStudents);
-
-        // jsonStudents deserialization to new collection (5)
-        List<Student> deserializedStudentsCollection = JsonUtil.StudentsFromJson(jsonStudents);
-
-        // check number of elements in collections (before and after deserialization) (6)
-        if (students.size() != deserializedStudentsCollection.size()) {
-            System.out.println("Number of objects before deserialization (" + students.size() + ") and after (" +
-                    deserializedStudentsCollection.size() + ") are different.");
-        }
-
-        // Check ser/deser for single objects
-        students.
-                stream().
-                map(JsonUtil::StudentToJson).
-                map(JsonUtil::StudentFromJson).
-                forEach(System.out::println);
+//        CompareUniversities = getUniversityComparator(UniversityComparator.BY_ID);
+//        List<University> universities = xlsxReader.readUniversity().stream().sorted(compareUniversities).collect(Collectors.toList());
+//
+//        // universities collection serialization (4)
+//        String jsonUniversities = JsonUtil.UniversitiesToJson(universities);
+//        System.out.println(jsonUniversities);
+//
+//        // jsonUniversities deserialization to new collection (5)
+//        List<University> deserializedUniversitiesCollection = JsonUtil.UniversitiesFromJson(jsonUniversities);
+//
+//        // check number of elements in collections (before and after deserialization) (6)
+//        if (universities.size() != deserializedUniversitiesCollection.size()) {
+//            System.out.println("Number of objects before deserialization (" + universities.size() + ") and after (" +
+//                    deserializedUniversitiesCollection.size() + ") are different.");
+//        }
+//
+//        // Check ser/deser for single objects
+//        universities.
+//                stream().
+//                map(JsonUtil::UniversityToJson).
+//                map(JsonUtil::UniversityFromJson).
+//                forEach(System.out::println);
+//
+//        System.out.println("--------------------------------------------------------");
+//
+//        CompareStudents compareStudents = getStudentComparator(StudentComparator.By_AVG_EXAM_SCORE_REVERSE);
+//        List<Student> students = xlsxReader.readStudent().stream().sorted(compareStudents).collect(Collectors.toList());
+//
+//        // students collection serialization (4)
+//        String jsonStudents = JsonUtil.StudentsToJson(students);
+//        System.out.println(jsonStudents);
+//
+//        // jsonStudents deserialization to new collection (5)
+//        List<Student> deserializedStudentsCollection = JsonUtil.StudentsFromJson(jsonStudents);
+//
+//        // check number of elements in collections (before and after deserialization) (6)
+//        if (students.size() != deserializedStudentsCollection.size()) {
+//            System.out.println("Number of objects before deserialization (" + students.size() + ") and after (" +
+//                    deserializedStudentsCollection.size() + ") are different.");
+//        }
+//
+//        // Check ser/deser for single objects
+//        students.
+//                stream().
+//                map(JsonUtil::StudentToJson).
+//                map(JsonUtil::StudentFromJson).
+//                forEach(System.out::println);
     }
 }
