8b4c0198fcd97f2fd56ac4462199ecb7647cd9da
maxim <maxim.bienemann@gmail.com>
2022-10-15 16:52:24 +0200

    Tests of news

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,10 @@ import com.example.model.Student;
 import com.example.model.University;
 import com.example.util.CreateStatisticsUtil;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main
@@ -16,6 +18,12 @@ public class Main
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     public static void main( String[] args )
     {
+        try {LogManager.getLogManager().readConfiguration(
+                    Main.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Problem with logger configuration: " + e);
+        }
+
         LOGGER.log(Level.INFO, "Program was started");
         String pathUniversityInfo = "src/main/resources/universityInfo.xlsx";
         String pathUniversityStatistics = "src/main/resources/universityStatistics.xlsx";
